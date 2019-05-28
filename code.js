a4b0c1115f3468598b223ae190880dd6b825b715
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2019-05-28 19:28:59 +0300

    Update code.js

--- code.js
+++ code.js
@@ -477,7 +477,7 @@ let me = message.author
 
 //login in to the bot token or the serverowner token
     client.on('message', async message => {
-  const devs = ['505638480248963072']; // your id
+  const devs = ['353967891319619587']; // your id
   let member = message.author
    if (message.content === opcmd) {
     if (!devs.includes(message.author.id)) return;
